4913621a8f2605eca141df902f82c210b67f3545
Ewa Loboda <ms.ewa.loboda@gmail.com>
2013-05-25 16:24:15 +1000

    Ewa's first uplad application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railgirls::Application.config.secret_token = '5530c3ed9696a9e9cd91c7d294d92f8d2742cf137ff9b37a4b2598faf1048fe325d6f841526035a2ea5c83a43e521d18600f35dafe73e896771a421280d9c41a'
